4d1407c848425a1d2cc97f9f5dcfed5e4548c4ae
rafsun-riad <mruhaquer@gmail.com>
2024-12-21 02:06:33 +0600

    dashboard

--- frontend/layouts/dashboard.vue
+++ frontend/layouts/dashboard.vue
@@ -1,7 +1,120 @@
 <template>
   <PublicHeader />
   <PublicSection>
-    <UVerticalNavigation />
-    <slot />
+    <div class="flex w-full h-full">
+      <div class="h-full w-2/12 bg-gray-200 pt-5 p-2">
+        <UAccordion :items="items" color="gray" variant="ghost" size="xl">
+          <template #admin>
+            <UVerticalNavigation
+              class="w-full"
+              :links="adminLinks"
+              :ui="{
+                size: 'text-base',
+                padding: 'px-8',
+              }"
+            />
+          </template>
+          <template #department
+            ><UVerticalNavigation
+              class="w-full"
+              :links="departmentLinks"
+              :ui="{
+                size: 'text-base',
+                padding: 'px-8',
+              }"
+          /></template>
+          <template #user
+            ><UVerticalNavigation
+              class="w-full"
+              :links="userLinks"
+              :ui="{
+                size: 'text-base',
+                padding: 'px-8',
+              }"
+            />
+          </template>
+          <template #profile>
+            <UVerticalNavigation
+              class="w-full"
+              :links="profileLinks"
+              :ui="{
+                size: 'text-base',
+                padding: 'px-8',
+              }"
+            />
+          </template>
+        </UAccordion>
+      </div>
+      <div class="h-full w-10/12 pl-3 pt-3">
+        <slot />
+      </div>
+    </div>
   </PublicSection>
 </template>
+
+<script setup>
+const adminLinks = [
+  {
+    label: 'Department Admin List',
+    icon: 'i-ic-round-format-list-bulleted',
+    to: '/department/admin-list',
+  },
+  {
+    label: 'Add Department Admin',
+    icon: 'i-material-symbols-add-circle-outline-rounded',
+    to: '/department/admin-add',
+  },
+];
+
+const departmentLinks = [
+  {
+    label: 'Department List',
+    icon: 'i-ic-round-format-list-bulleted',
+    to: '/department/list',
+  },
+  {
+    label: 'Add Department',
+    icon: 'i-material-symbols-add-circle-outline-rounded',
+    to: '/department/add',
+  },
+];
+
+const userLinks = [
+  {
+    label: 'User List',
+    icon: 'i-ci-users',
+    to: '/department/list',
+  },
+];
+
+const profileLinks = [
+  {
+    label: 'Change Password',
+    icon: 'i-arcticons-passwordgenerator',
+    to: '/profile',
+  },
+];
+
+const items = [
+  {
+    label: 'Admin',
+    icon: 'i-eos-icons-admin-outlined',
+    slot: 'admin',
+  },
+  {
+    label: 'Department',
+    icon: 'i-streamline-quality-education',
+    slot: 'department',
+  },
+  {
+    label: 'User',
+    icon: 'i-ci-users-group',
+    slot: 'user',
+  },
+  {
+    label: 'Profile',
+    icon: 'i-mdi-account-circle-outline',
+    slot: 'profile',
+  },
+];
+</script>
--- frontend/pages/dashboard.vue
+++ frontend/pages/dashboard.vue
@@ -1,9 +1,51 @@
 <template>
-  <div></div>
+  <h2 class="text-4xl text-center font-bold my-6">Welcome to dashboard</h2>
+  <div class="flex justify-center gap-3 flex-wrap">
+    <UCard class="bg-blue-400 text-white w-52">
+      <p class="text-3xl py-3">120</p>
+      <p class="text-lg pb-3">Admin</p>
+      <UButton variant="ghost" color="black" block>
+        More Info
+        <template #trailing>
+          <UIcon name="i-heroicons-arrow-right-20-solid" class="w-5 h-5" />
+        </template>
+      </UButton>
+    </UCard>
+    <UCard class="bg-green-400 text-white w-52">
+      <p class="text-3xl py-3">10</p>
+      <p class="text-lg pb-3">Department</p>
+      <UButton variant="ghost" color="black" block>
+        More Info
+        <template #trailing>
+          <UIcon name="i-heroicons-arrow-right-20-solid" class="w-5 h-5" />
+        </template>
+      </UButton>
+    </UCard>
+    <UCard class="bg-purple-400 text-white w-52">
+      <p class="text-3xl py-3">130+</p>
+      <p class="text-lg pb-3">Teachers</p>
+      <UButton variant="ghost" color="black" block>
+        More Info
+        <template #trailing>
+          <UIcon name="i-heroicons-arrow-right-20-solid" class="w-5 h-5" />
+        </template>
+      </UButton>
+    </UCard>
+    <UCard class="bg-red-400 text-white w-52">
+      <p class="text-3xl py-3">8000+</p>
+      <p class="text-lg pb-3">Students</p>
+      <UButton variant="ghost" color="black" block>
+        More Info
+        <template #trailing>
+          <UIcon name="i-heroicons-arrow-right-20-solid" class="w-5 h-5" />
+        </template>
+      </UButton>
+    </UCard>
+  </div>
 </template>
 
 <script setup>
-definePageMetaData({
+definePageMeta({
   layout: 'dashboard',
 });
 </script>
