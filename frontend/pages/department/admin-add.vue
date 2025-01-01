d953286dfbef6fb96170007878cd14ed84923e72
rafsun-riad <mruhaquer@gmail.com>
2025-01-01 23:41:51 +0600

    super admin dashboard

--- frontend/pages/department/admin-add.vue
+++ frontend/pages/department/admin-add.vue
@@ -1,9 +1,57 @@
 <template>
-  <div>Add Department Admin</div>
+  <UContainer>
+    <h2 class="text-center text-4xl my-6">Add Department Admin</h2>
+    <form @submit.prevent="handleSubmit">
+      <div class="max-w-lg w-full mx-auto space-y-3">
+        <UFormGroup label="Email" required>
+          <UInput
+            color="green"
+            type="email"
+            size="lg"
+            required
+            placeholder="Enter Email"
+          />
+        </UFormGroup>
+        <UFormGroup label="Password" required>
+          <UInput
+            color="green"
+            type="password"
+            size="lg"
+            placeholder="Enter Password"
+          />
+        </UFormGroup>
+        <UFormGroup label="Confirm Password" required>
+          <UInput
+            color="green"
+            type="password"
+            size="lg"
+            placeholder="Enter Confirm Password"
+          />
+        </UFormGroup>
+        <UFormGroup label="Department" required>
+          <USelectMenu
+            color="green"
+            placeholder="Select Department"
+            size="lg"
+          />
+        </UFormGroup>
+
+        <UButton
+          type="submit"
+          label="Add Department Admin"
+          size="lg"
+          color="green"
+          block
+        />
+      </div>
+    </form>
+  </UContainer>
 </template>
 
 <script setup>
 definePageMeta({
   layout: "dashboard",
 });
+
+async function handleSubmit() {}
 </script>
---
+++ frontend/pages/profile.vue
@@ -0,0 +1,49 @@
+<template>
+  <UContainer>
+    <h2 class="text-center text-4xl my-6">Change Your Password</h2>
+    <div class="max-w-md w-full mx-auto space-y-3">
+      <UFormGroup label="Old Password">
+        <UInput
+          color="green"
+          type="password"
+          size="lg"
+          required
+          placeholder="Enter Old Password"
+        />
+      </UFormGroup>
+      <UFormGroup label="New Password">
+        <UInput
+          color="green"
+          type="password"
+          size="lg"
+          placeholder="Enter New Password"
+        />
+      </UFormGroup>
+      <UFormGroup label="Confirm New Password">
+        <UInput
+          color="green"
+          type="password"
+          size="lg"
+          placeholder="Confirm New Password"
+        />
+      </UFormGroup>
+
+      <UButton
+        type="button"
+        label="Change Password"
+        size="lg"
+        color="green"
+        block
+        @click.prevent="handlePasswordChange"
+      />
+    </div>
+  </UContainer>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "dashboard",
+});
+
+async function handlePasswordChange() {}
+</script>
